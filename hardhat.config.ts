aaf2beb269f6933317d4af8440aa85a43e32ca9d
shuhei tanaka <shuhei46491@gmail.com>
2024-11-15 21:20:25 +0700

    chore: forge install openzeppelin-upgradeable

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,64 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-foundry";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-storage-layout";
+import "hardhat-contract-sizer";
+import "hardhat-storage-layout-changes";
+import "hardhat-abi-exporter";
+import "hardhat-gas-reporter";
+import "@nomicfoundation/hardhat-ignition";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.27",
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      viaIR: false,
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      blockGasLimit: 10_000_000,
+      accounts: {
+        count: 10,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    gasPrice: 1,
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: false,
+    strict: true,
+  },
+  paths: {
+    storageLayouts: ".storage-layouts",
+  },
+  storageLayoutChanges: {
+    contracts: [],
+    fullPath: false,
+  },
+  abiExporter: {
+    path: "./exported/abi",
+    runOnCompile: true,
+    clear: true,
+    flat: true,
+    only: ["Lootery"],
+    except: ["test/*"],
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
 
 export default config;
